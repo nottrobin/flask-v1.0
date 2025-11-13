09f4ca460542a234c4a9aacaded8553be5a1f781
Robin Winslow Morris <robin@robinwinslow.co.uk>
2025-11-13 01:53:14 +0000

    Version 1.0.5 - continuing on from where Flask left off

--- setup.py
+++ setup.py
@@ -4,48 +4,25 @@ import re
 from setuptools import find_packages
 from setuptools import setup
 
-with io.open("README.rst", "rt", encoding="utf8") as f:
+with io.open("README.md", "rt", encoding="utf8") as f:
     readme = f.read()
 
 with io.open("flask/__init__.py", "rt", encoding="utf8") as f:
     version = re.search(r"__version__ = \'(.*?)\'", f.read()).group(1)
 
 setup(
-    name="Flask",
+    name="Flask-v1.0",
     version=version,
-    url="https://palletsprojects.com/p/flask/",
+    url="https://github.com/nottrobin/flask-v1.0",
     project_urls={
-        "Documentation": "http://flask.palletsprojects.com/",
-        "Code": "https://github.com/pallets/flask",
-        "Issue tracker": "https://github.com/pallets/flask/issues",
+        "Documentation": "https://github.com/nottrobin/flask-v1.0/blob/main/README.md",
+        "Code": "https://github.com/nottrobin/flask-v1.0"
     },
     license="BSD-3-Clause",
-    author="Armin Ronacher",
-    author_email="armin.ronacher@active-4.com",
-    maintainer="Pallets",
-    maintainer_email="contact@palletsprojects.com",
+    author="Robin Winslow Morris",
+    author_email="robin@robinwinslow.co.uk",
     description="A simple framework for building complex web applications.",
     long_description=readme,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Flask",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
     packages=find_packages(),
     include_package_data=True,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
@@ -55,23 +32,5 @@ setup(
         'itsdangerous >= 0.21, < 2.1',
         'click>=2.0',
     ],
-    extras_require={
-        "dotenv": ["python-dotenv"],
-        "dev": [
-            "pytest",
-            "coverage",
-            "tox",
-            "sphinx",
-            "pallets-sphinx-themes",
-            "sphinxcontrib-log-cabinet",
-            "sphinx-issues",
-        ],
-        "docs": [
-            "sphinx",
-            "pallets-sphinx-themes",
-            "sphinxcontrib-log-cabinet",
-            "sphinx-issues",
-        ],
-    },
     entry_points={"console_scripts": ["flask = flask.cli:main"]},
 )
